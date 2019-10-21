b03323fe5f9b9c85aa7baad47e75b17929912953
whitebearcoder <denisvaran@outlook.com>
2019-10-21 06:54:48 -0700

    new navbar for Desktop & Tablet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import LayoutBlock from "./layout/LayoutBlock";
 import LogoImg from "../images/Logo.svg";
 import ButtonsRow from "./layout/ButtonsRow";
 import colors from "./style/colors";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 const HeaderWrapper = styled.div`
-  ${LayoutBlock}
   display: flex;
-  background: white;
-  overflow: hidden;
-  @media (max-width: 479px) {
-    flex-direction: column;    
-  }  
-
-  div.left {
-    flex: 0 0.8 auto;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    @media (max-width: 600px) {
-      padding: 0.5em 0.5em;
-      img {
-        width: 10em;
-      }
-    }
-    @media (max-width: 479px) {
-      padding: 1em 2em;
-      justify-content: center;
-      img {
-        width: 15em;
-      }
-    }    
-  }
-  div.right {
-    flex: 0 0.5 auto;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    @media (max-width: 600px) {
-      padding: 0.5em 0.5em;     
-    }
-    @media (max-width: 479px) {
-      padding: 0 2em 1em;
-      justify-content: center;
-    }
+  background: white;  
+  @media (max-width: 1199px) {
+    flex-direction: column;
   }
 `;
 
-const Space = styled.div`
-  width: 1.5em;
-  @media (max-width: 600px) {
-    display: none;
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 1em 2em;
+  @media (max-width: 1199px) {
+    justify-content: center;
+    padding-bottom: 0;
   }
 `;
 
@@ -60,6 +30,7 @@ const Logo  = styled.img`
 `;
 
 const StyledSignUpButton = styled.a`
+  position: relative;
   background: ${props => colors[props.color] || colors.accent1};
   border: 2px solid ${props => colors[props.color] || colors.accent1};
   color: white;
@@ -68,7 +39,7 @@ const StyledSignUpButton = styled.a`
   font-size: 1.05em;
   outline: none;
   cursor: pointer;
-  text-decoration: none;
+  text-decoration: none;  
   :active {
     filter: brightness(0.8);
   }
@@ -85,22 +56,93 @@ const StyledSignUpButton = styled.a`
     border: none;
     color: ${colors.grey};
   `}
+
+  &.has-sub-menu {
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    .fa-angle-down {
+      margin-left: 5px;    
+      transform: translateY(2px);
+    }
+  
+    .dropdown {
+      position: absolute;
+      display: none;
+      top: 100%;
+      left: 0;
+      padding: 0 1em;
+      background-color: white;
+      margin-top: 2px;
+      li {
+        padding: 1em 0;
+        text-decoration: none;
+        white-space: nowrap;
+        &:hover {
+          color: #6B50C8;
+        }
+      }
+    }
+  
+    &:hover {
+      .dropdown {
+        display: block;
+      }
+    }
+  } 
+  
+  @media (max-width: 1280px) {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+  }
+
+  @media (max-width: 1199px) {
+
+  }
+`;
+
+const NavBar = styled(ButtonsRow)`
+  margin-left: auto;
+  padding: 0 2em;
+  z-index: 1;
+  align-items: center;
+  @media (max-width: 1199px) {
+    justify-content: center;
+    margin: 0 auto;
+  }
+
+  .register-btn-signup {
+    margin: 1em;
+  }
 `;
 
 const Header = props => {
   return (
     <HeaderWrapper>
-      <div className="left">
+      <LogoContainer>
         <a href="https://www.heyagenda.com/"><Logo src={LogoImg} /></a>        
-      </div>
-      <div className="right">
-        <ButtonsRow>
-          <StyledSignUpButton type="tertiary" href="http://help.heyagenda.com/support/kb#/categories">Help</StyledSignUpButton>
-          <Space />
-          <StyledSignUpButton type="secondary" id="login-btn" href="#">Login</StyledSignUpButton>
-          <StyledSignUpButton className="register-btn-header register-btn-signup" href="#">Sign Up</StyledSignUpButton>
-        </ButtonsRow>
-      </div>
+      </LogoContainer>
+      <NavBar>
+        <StyledSignUpButton type="tertiary" href="http://help.heyagenda.com/support/kb#/categories">Home</StyledSignUpButton>
+        <StyledSignUpButton type="tertiary" href="http://help.heyagenda.com/support/kb#/categories">Features</StyledSignUpButton>
+        <StyledSignUpButton type="tertiary" href="http://help.heyagenda.com/support/kb#/categories">Integrations</StyledSignUpButton>
+        <StyledSignUpButton className="has-sub-menu" type="tertiary">
+          Examples <FontAwesomeIcon icon={faAngleDown} />
+          <ul className="dropdown">
+            <li>Meeting Rooms</li>
+            <li>Sports Venue</li>
+            <li>Professional Studios</li>
+            <li>Coworking Spaces</li>
+            <li>Classrooms and Labs</li>
+            <li>Consulting Rooms</li>
+          </ul>
+        </StyledSignUpButton>
+        <StyledSignUpButton type="tertiary" href="http://help.heyagenda.com/support/kb#/categories">Pricing</StyledSignUpButton>
+        <StyledSignUpButton type="tertiary" href="http://help.heyagenda.com/support/kb#/categories">More</StyledSignUpButton>
+        <StyledSignUpButton type="tertiary" id="login-btn" href="#">Login</StyledSignUpButton>
+        <StyledSignUpButton className="register-btn-header register-btn-signup" href="#">Sign Up</StyledSignUpButton>
+      </NavBar>
     </HeaderWrapper>
   );
 };
